96ba18c72b1c784e9b50a92f448f71784b9ed2c7
James Sedlacek <jamessedlacek@Jamess-MacBook-Pro.local>
2022-03-05 17:56:19 -0500

    Views folder structure, UFOTableViewCell init

---
+++ UFOsightings/UFOsightings/Views/UFO/UFOTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UFOTableViewCell.swift
+//  UFOsightings
+//
+//  Created by James Sedlacek on 3/5/22.
+//
+
+import UIKit
+
+class UFOTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
