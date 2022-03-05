542c00232aebae0bba6e4692c1da38a057d8ce18
James Sedlacek <jamessedlacek@Jamess-MacBook-Pro.local>
2022-03-05 17:30:47 -0500

    init project

---
+++ UFOsightings/UFOsightings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UFOsightings
+//
+//  Created by James Sedlacek on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
