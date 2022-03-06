f3032de70eda3645e351ecd6024df8724f090462
James Sedlacek <jamessedlacek@Jamess-MacBook-Pro.local>
2022-03-05 21:56:57 -0500

    MainVC completed

--- UFOsightings/UFOsightings/Views/Main/MainVC.swift
+++ UFOsightings/UFOsightings/Views/Main/MainVC.swift
@@ -19,7 +19,8 @@ class MainVC: UIViewController {
 
     // MARK: Properties
     
-    fileprivate var currentTab: TabOption = .strangeFlyers
+    fileprivate var ufoVM = UFOSightingsVM()
+    fileprivate let nibIdentifier = "UFOTableViewCell"
     
     // MARK: Outlets
     
@@ -28,14 +29,12 @@ class MainVC: UIViewController {
     // MARK: Actions
     
     @IBAction func plusTapped(_ sender: UIBarButtonItem) {
-        // TODO: Add an entry with random attributes
-        // corresponding to the tab the user is on
+        ufoVM.appendRandom()
     }
     
     @IBAction func tabChanged(_ sender: UISegmentedControl) {
         if let selectedTab = TabOption(rawValue: sender.selectedSegmentIndex) {
-            currentTab = selectedTab
-            tableView.reloadData()
+            ufoVM.updateTab(to: selectedTab)
         }
     }
     
@@ -44,11 +43,26 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupVM()
     }
+    
+    // MARK: Setup
     
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(UINib(nibName: nibIdentifier, bundle: nil),
+                           forCellReuseIdentifier: nibIdentifier)
+    }
+    
+    private func setupVM() {
+        ufoVM.reloadTableViewClosure = {
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+        }
+        
+        ufoVM.fetchSightings()
     }
 }
 
@@ -57,11 +71,35 @@ class MainVC: UIViewController {
 extension MainVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return ufoVM.numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: nibIdentifier, for: indexPath) as? UFOTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.dateLabel.text = ufoVM.getDate(for: indexPath.row)
+        cell.timeLabel.text = ufoVM.getTime(for: indexPath.row)
+        cell.speedLabel.text = ufoVM.getSpeed(for: indexPath.row)
+        cell.ufoTypeLabel.text = ufoVM.getType(for: indexPath.row).rawValue
+        cell.ufoImageView.image = ufoVM.getImage(for: indexPath.row)
+        
+        return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 48
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            ufoVM.delete(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
